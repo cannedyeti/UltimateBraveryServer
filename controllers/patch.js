54bea59cfd8b7479af243a721247c25cd9f44205
cannedyeti <cpotebnya@gmail.com>
2022-01-28 14:59:34 -0800

    update helpers and patch

--- controllers/patch.js
+++ controllers/patch.js
@@ -2,25 +2,26 @@ const fetch = require('node-fetch')
 const db = require('_helpers/db');
 const Patch = db.Patch;
 
+const LEAGUE_VERSION_API = 'https://ddragon.leagueoflegends.com/api/versions.json';
+
 const  getPatch = async (req, res) => {
-    console.log("get patch")
     const currentPatch = await Patch.findOne().sort({ field: 'asc', _id: -1 }).limit(1)
     res.json(currentPatch)
 }
 
+
 const updatePatch = async (req, res) => {
-    console.log("update patch")
-    res.send('updatePatch')
     let newPatch;
-    await fetch('https://ddragon.leagueoflegends.com/api/versions.json')
+    await fetch(LEAGUE_VERSION_API)
     .then((res) => res.json())
     .then((json)=> {
-        console.log({json})
         newPatch = json[0];
+        Patch.findOrCreate({patchNumber: newPatch}, (err, res) => {
+            return res;
+        })
+        res.json(newPatch);
     })
-    Patch.findOrCreate({patchNumber: newPatch}, (err, res) => {
-        return res;
-    })
+    .catch((err) => console.log({err}))
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
 
 app.use(bodyParser.urlencoded({ extended: false }));
